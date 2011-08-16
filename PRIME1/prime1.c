25224443e47a52561c5c96ed12c0ae715d1f6a3e
Younggun, Kim <scari.net@gmail.com>
2011-08-16 09:03:31 +0900

    Sieve algorithm and use sqaure to verify the number is prime or not

--- PRIME1/prime1.c
+++ PRIME1/prime1.c
@@ -1,93 +1,67 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
+#define PRIME	(1)
+#define NPRIME	(0)
 // return true if n is prime
-int
-is_prime (int n)
-{
+unsigned char *sieve;
+
+int is_prime (int n) {
     int i = 2;
+	int sqrt_n = 0;
 
     if (1 >= n)
-        return 0;
+        return NPRIME;
 
-    for (i = 2; i < n; i++) {
+	sqrt_n = (int) sqrt((double) n) + 1;
+
+    for (i = 2; i < sqrt_n; i++) {
         if (!(n % i))
-            return 0;
+            return NPRIME;
     }
-    return 1;
-}
-int
-mark_multiple_of (int index, int multiple_of, int min) {
-    //printf ("(%d+%d) %% %d\n", index, min, multiple_of);
-    if ((index+min) == 1)
-        return -1;
-
-    if ((index+min) == multiple_of)
-        return 1;
-
-    if (((index+min)%multiple_of) == 0)
-        return -1;
+    return PRIME;
 }
 
-void
-show_primes (int m, int n) {
+void fill_sieve(int m, int n, int len) {
+	int i = 0;
+	int j = 0;
+	if (m == 1)
+		sieve[0] = NPRIME;
+
+	for (i = 0; i < len; i++) {
+		if (sieve[i] == PRIME && is_prime(i+m) > 0) {
+			for (j = (i+m)*2; j <= len; j += (i+m))
+				sieve[j-1] = NPRIME;
+		} else {
+			sieve[i] = NPRIME;
+		}
+	}
+}
+
+void show_primes (int m, int n) {
     int i = 0;
-    int j = 0;
-    int k = 0;
-    int l = 0;
     int len = 0;
-    int *array = NULL;
 
     // make M <= N
     if (m > n)
         m ^= n ^= m ^= n;   // swap
 
     len = n - m + 1;
-    array = (int *) malloc (sizeof (int) * len);
-    if (NULL == array) {
-        printf ("malloc failed!\n");
-        return;
-    }
-    memset(array, 1, len);
+	sieve = (unsigned char *) malloc (sizeof (unsigned char) * len);
 
-    // erathostenes's sieve
-    for (i = 0; i < len; i++) {
-        for (j = m; j > 1; j--) {
-            if (array[i] >= 0) {
-                if (0 > mark_multiple_of (i, j, m)) {
-                    array[i] = -1;
-                    /*
-                    for (k = i; k < len; k += k) {
-                        array[k] = -1;
-                    }
-                    */
-                }
-            }
-        }
-        for (j = m; j <= (n/2); j++) {
-            if (array[i] >= 0 && j > 1) {
-                if (0 > mark_multiple_of (i, j, m)) {
-                    array[i] = -1;
-                    /*
-                    for (k = i; k < len; k += k) {
-                        array[k] = -1;
-                    }
-                    */
-                }
-            }
-        }
-    }
+	memset (sieve, PRIME, len);
+	fill_sieve(m, n, len);
     // show the prime
     for (i = 0; i < len; i++) {
-        if (array[i] != -1)
+        //if (is_prime(i + m))
+        if (sieve[i] == PRIME)
             printf ("%d\n", i + m);
     }
-    free (array);
+	free (sieve);
 }
 
-int
-main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
     int testcase = 0;
     int m = 0;
     int n = 0;
