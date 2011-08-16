78189bdb4d10b5c10f1bf64abbfe176f2fdf9a87
Younggun, Kim <scari.net@gmail.com>
2011-08-17 08:19:02 +0900

    ONP updated

--- ONP/onp.c
+++ ONP/onp.c
@@ -2,15 +2,18 @@
 #define EXP_LEN	(400)
 
 char*
-rnp (const char *exp)
+rnp (const char *exp, char *ret)
 {
 	int len = strlen (exp);
-	int i = 0;
+	int i = 0, rnp_idx = 0;
 	char c = '\0';
 	for (i = 0; i < len; i++) {
 		c = exp[i];
 
 		switch (c) {
+			case 'a'...'z':
+				ret[rnp_idx++] = c;
+				break;
 			case '*':
 			case '/':
 			case '^':
@@ -22,19 +25,21 @@ rnp (const char *exp)
 				break;
 		}
 	}
-	return NULL;
+	return ret;
 }
 int
 main (int argc, char *argv[])
 {
 	int testcases = 0;
 	char exp[EXP_LEN] = { 0, };
+	char ret[400] = { 0, };
 	scanf ("%d", &testcases);
 	while (testcases--)
 	{
 		memset (exp, 0, EXP_LEN);
+		memset (ret, 0, EXP_LEN);
 		scanf ("%s", exp);
-		printf ("%s\n", rnp(exp));
+		printf ("%s\n", rnp(exp, ret));
 		//printf ("%s\n", exp);
 	}
 	return 0;
