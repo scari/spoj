21b070c3067a28be7644a6cab9b2d765dddb25cc
Younggun, Kim <scari.net@gmail.com>
2011-08-18 05:41:55 +0900

    Accepted!

--- PALIN/palin.py
+++ PALIN/palin.py
@@ -1,45 +1,90 @@
 #!/usr/bin/env python
+num_map = { '0': '1', 
+			'1': '2',
+			'2': '3',
+			'3': '4',
+			'4': '5',
+			'5': '6',
+			'6': '7',
+			'7': '8',
+			'8': '9',
+			'9': '0'}
+
+def memoize(fn):
+	cached = {}
+	def wrap(number):
+		if number not in cached:
+			cached[number] = fn(number)
+		return cached[number]
+	return wrap
+
+@memoize
+def inc(n):
+	carry = 1
+	temp = []
+	for digit in n[::-1]:
+		if carry == 1:
+			temp.append(num_map[digit])
+			if digit == '9':
+				carry = 1
+			else:
+				carry = 0
+		else:
+			temp.append(digit)
+
+	if carry == 1:
+		temp.append('1')
+	
+	temp.reverse()
+	return "".join(temp)
+
+
 def palindrome(n):
 	length = len(n)
 	head = n[:length/2]
 	if length == 1:
-		if int(n) == 9:
+		if n == '9':
 			return 11
 		else:
-			return int(n) + 1
+			return num_map[n] 
 
 	if length % 2 == 0:
-		if int(head[::-1]) > int(n[length/2:]):
+		if head[::-1] > n[length/2:]:
 			return "%s%s" % (head, head[::-1])
 		else:
 			old_len = len(head)
-			head = str(int(head) + 1);
+			head = inc(head)
 			new_len = len(head)
-			middle = 0
-			tail = 0
+			middle = ''
+			tail = ''
 			if old_len < new_len:
 				middle = head[-1]
-				head = head[:new_len/2+1]
+				head = head[:new_len/2]
 				return "%s%s%s" % (head, middle, head[::-1])
 			else:
 				return "%s%s" % (head, head[::-1])
 					
 	else:
-		if int(head[::-1]) > int(n[length/2+1:]):
-			return "%s%s%s" % (head, str(n)[length/2], head[::-1])
+		if head[::-1] > n[length/2+1:]:
+			return "%s%s%s" % (head, n[length/2], head[::-1])
 		else:
-			middle = int(n[length/2]) + 1
-			if middle == 10:
-				head = str(int(head) + 1);
-				middle = 0;
-				return "%s%s" % (head, head[::-1])
+			middle = inc(n[length/2])
+			if middle == '10':
+				old_len = len(head)
+				head = inc(head)
+				middle = '0';
+				new_len = len(head)
+				if old_len < new_len:
+					return "%s%s" % (head, head[::-1])
+				else:
+					return "%s%s%s" % (head, middle, head[::-1])
 			else:
 				return "%s%s%s" % (head, middle, head[::-1])
 
 if __name__ == '__main__':
-    testcases = int(raw_input().strip())
-    while (testcases > 0):
-        n = raw_input().strip()
-        print (palindrome(n))
-        testcases -= 1
+	testcases = int(raw_input().strip())
+	while (testcases > 0):
+		n = raw_input().strip()
+		print (palindrome(n))
+		testcases -= 1
 
